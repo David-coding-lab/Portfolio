99e6516d6b2ca42a3324af52080539e2844b573a
Care Tech Hub <caretechhub@gmail.com>
2024-10-22 16:54:03 +0100

    adding responsiveness

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,10 +4,31 @@ import LinkedInd from '../assets/LinkedIn.svg'
 import Tweeter from '../assets/Twitter.svg'
 import FaceBook from '../assets/face book.png'
 import { desgignSystem } from '../App'
+import { useEffect, useState } from 'react'
 
 function NavBar({setActiveTab}) {
+    const [changeDisplay,setChangeDisplay] = useState(false)
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 950) {
+                setChangeDisplay(true);
+            } else {
+                setChangeDisplay(false);
+            }
+        };
+
+        handleResize();
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup the event listener on component unmount
+        return () => window.removeEventListener('resize', handleResize);
+      }, []);  // Empty dependency array so this only runs once on mount
+    console.log(changeDisplay);
+
+
     return (
-        <Flex width='flex' h='41px' m='auto' mt='16px' justifyContent='space-between' fontFamily={desgignSystem.designFont}>
+        <Flex width='flex' h='41px' m='auto' mt='16px' justifyContent={changeDisplay ? 'center':'space-between'} fontFamily={desgignSystem.designFont}>
         <Tabs variant="unstyled">
             <TabList gap="70px">
                 <Tab
@@ -38,19 +59,20 @@ function NavBar({setActiveTab}) {
         </Tabs>
 
 
-            <Flex gap='25px'>
-                <IconButton borderRadius='50%' bgColor='transparent'><Image src={LinkedInd}/></IconButton>
-                <IconButton borderRadius='50%' bgColor='transparent'><Image src={FaceBook}/></IconButton>
-                <IconButton borderRadius='50%' bgColor='transparent'><Image src={Tweeter}/></IconButton>
-                <Button
-                    bgColor={desgignSystem.primaryColor}
-                    color='white'
-                    fontWeight='medium'
-                    borderRadius='4px'
-                    w='152px'
-                    _hover={{opacity: .6}}
-                >Download Cv</Button>
-            </Flex>
+            {!changeDisplay &&
+                <Flex gap='25px'>
+                    <IconButton borderRadius='50%' bgColor='transparent'><Image src={LinkedInd}/></IconButton>
+                    <IconButton borderRadius='50%' bgColor='transparent'><Image src={FaceBook}/></IconButton>
+                    <IconButton borderRadius='50%' bgColor='transparent'><Image src={Tweeter}/></IconButton>
+                    <Button
+                        bgColor={desgignSystem.primaryColor}
+                        color='white'
+                        fontWeight='medium'
+                        borderRadius='4px'
+                        w='152px'
+                        _hover={{opacity: .6}}
+                    >Download Cv</Button>
+            </Flex>}
         </Flex>
     )
 }
